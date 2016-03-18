848465f54a3b0b443f833828f8c370c6d29f4ee1
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-18 16:06:28 -0700

    more styles and jss

--- src/js/index.js
+++ src/js/index.js
@@ -13,8 +13,8 @@ var HeadingScrollAnimation = (function() {
 
 	function animation() {
 		var scrolled		= window.pageYOffset;
-		var translateY	= scrolled / 3;
-		var opacity			= 1 - (scrolled / 600);
+		var translateY	= scrolled / 2;
+		var opacity			= 1 - (scrolled / 700);
 
 		heading.style.transform = 'translateY(' + translateY + 'px)';
 		heading.style.opacity = opacity;
@@ -26,7 +26,44 @@ var HeadingScrollAnimation = (function() {
 
 }());
 
+var WorkScrollAnimation = (function() {
+
+	var lists = [].slice.call(document.querySelectorAll('.work__list-item'));
+
+	function init() {
+		window.addEventListener('scroll', handleScroll);
+	}
+
+	function handleScroll() {
+		window.requestAnimationFrame(animation);
+	}
+
+	function animation() {
+		var scrolled = window.pageYOffset;
+
+		var offsets = lists.map(function(x) {
+			return x.getBoundingClientRect();
+		});
+
+		lists.forEach(function(list, i) {
+			var topVis = (offsets[i].top >= 0) && (offsets[i].top < window.innerHeight + 150);
+			var bottomVis = (offsets[i].bottom > 0) && (offsets[i].bottom <= window.innerHeight + 150);
+
+			if (topVis || bottomVis) {
+				list.classList.add('active');
+			} else {
+				list.classList.remove('active');
+			}
+		});
+	}
+
+	return {
+		init: init
+	};
+
+}());
 
 document.addEventListener('DOMContentLoaded', function() {
 	HeadingScrollAnimation.init();
+	WorkScrollAnimation.init();
 });
