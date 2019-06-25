bf0252d9cfe9bc000fb5bc4c3693a47a52649166
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-06-24 23:44:50 -0700

    add scroll animations and split work sections into files

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
+import sal from "sal.js";
+
 import { flowerAnimation } from "./flowerAnimation";
 import { workAccordion } from "./workAccordion";
 
 window.addEventListener("DOMContentLoaded", event => {
+  sal({
+    threshold: 0.36
+  });
   flowerAnimation();
   workAccordion();
 });
