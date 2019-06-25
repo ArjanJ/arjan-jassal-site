215418f075389ea7be56d599fa0a5c8136571cb2
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-06-24 22:33:15 -0700

    refactor workAccordion now that i know element.scrollHeight is a thing

--- scripts/workAccordion.js
+++ scripts/workAccordion.js
@@ -1,186 +1,65 @@
-import { debounce } from "./utils";
-
 /**
  * workAccordion
- * This code toggles the sections under "Work Experience".
+ * This code expands/collapses the work sections.
  */
 export const workAccordion = () => {
-  const ACCORDION_ID = "workAccordion";
-  const CONTENT_OPEN_CLASSNAME = "work-group-content--open";
-  const WORK_GROUP_CLASSNAME = "work-group";
-  const WORK_GROUP_CONTENT_CLASSNAME = "work-group-content";
-
-  /**
-   * heights: The heights of each accordion section when open.
-   * openShelves: The indices of open accordion sections.
-   */
-  const state = {
-    heights: [],
-    openShelves: []
-  };
-
-  const accordionElement = document.getElementById(ACCORDION_ID);
+  const SECTION_SELECTOR = ".work-group";
+  const CONTENT_SELECTOR = ".work-group-content";
 
   const elements = {
-    parent: accordionElement,
-    children: getChildren(accordionElement)
+    accordion: document.getElementById("workAccordion")
   };
 
-  const handleResize = debounce(() => {
-    state.heights = getHeights();
-    state.openShelves = [];
-    collapseHeightOfShelves();
-  }, 100);
-
-  init();
-
-  /**
-   * Add one event listener to the accordion element instead
-   * of each section within.
-   */
-  function setupEventListeners() {
-    elements.parent.addEventListener("click", handleClick, false);
-    window.addEventListener("resize", handleResize);
-  }
-
-  function getChildren(parent) {
-    if (parent && parent.children) {
-      return parent.children;
-    } else {
-      return [];
-    }
-  }
-
-  /**
-   * handleClick
-   * When a section within the accordion is clicked we check
-   * if it's open (if the index is in the state.openShelves array).
-   * If it's open we close it, and vice-versa.
-   */
-  function handleClick(event) {
-    const { openShelves } = state;
-    const { target } = event;
-    const workGroupElement = target.closest(`.${WORK_GROUP_CLASSNAME}`);
-    const workGroupContentElement = target.closest(
-      `.${WORK_GROUP_CONTENT_CLASSNAME}`
-    );
+  function expandSection(element) {
+    element.setAttribute("data-expanded", "true");
+    const content = element.querySelector(CONTENT_SELECTOR);
+    const backgroundColor = element.getAttribute("data-color");
 
     /**
-     * If we clicked on an element that has a parent of .work-group-content
-     * we don't want to do anything because the inner content should be
-     * clickable without the section closing or opening.
+     * scrollHeight is the height of the content
+     * inside of the element.
      */
-    if (!workGroupContentElement && workGroupElement) {
-      const index = parseInt(workGroupElement.getAttribute("data-index"), 10);
-      const isOpen = openShelves.includes(index);
-      const backgroundColor = workGroupElement.getAttribute("data-color");
-      const contentElement = workGroupElement.querySelector(
-        `.${WORK_GROUP_CONTENT_CLASSNAME}`
-      );
-
-      // Close the section.
-      if (isOpen) {
-        setHeight(getContentElement(workGroupElement));
-        contentElement.classList.remove(CONTENT_OPEN_CLASSNAME);
-        removeBackgroundColor();
-        return removeOpenShelf(index);
-      }
-
-      // Open the section.
-      contentElement.style.height = `${state.heights[index]}px`;
-      contentElement.classList.add(CONTENT_OPEN_CLASSNAME);
-      setBackgroundColor(backgroundColor);
-      return addOpenShelf(index);
-    }
+    content.style.height = `${content.scrollHeight}px`;
+    setBackgroundColor(backgroundColor);
   }
 
-  function addOpenShelf(index) {
-    state.openShelves = [...state.openShelves, index];
+  function collapseSection(element) {
+    element.setAttribute("data-expanded", "false");
+    const content = element.querySelector(CONTENT_SELECTOR);
+    content.removeAttribute("style");
+    document.body.removeAttribute("style");
   }
 
-  function removeOpenShelf(index) {
-    state.openShelves = state.openShelves.filter(x => x !== index);
-  }
+  function handleClick(event) {
+    const { target } = event;
+    const isCollapsibleContent = target.closest(CONTENT_SELECTOR);
 
-  /**
-   * getHeights
-   * Returns the height of content within each accordion section.
-   */
-  function getHeights() {
-    const { children } = elements;
-
-    if (children.length === 0) {
+    /**
+     * If the content inside of the collapsible section was clicked
+     * then we don't do anything because it would be bad UX if someone
+     * tried to highlight some text and it collapsed the section on click.
+     */
+    if (isCollapsibleContent) {
       return null;
     }
 
-    return Array.from(children).map(
-      child => getContentElement(child).offsetHeight
-    );
-  }
+    const section = target.closest(SECTION_SELECTOR);
+    const isExpanded = section.getAttribute("data-expanded") === "true";
 
-  /**
-   * collapseHeightOfShelves
-   * Visually closes all of the accordion sections.
-   */
-  function collapseHeightOfShelves() {
-    const { children } = elements;
-
-    if (children.length === 0) {
-      return null;
+    if (isExpanded) {
+      return collapseSection(section);
     }
 
-    const childrenArr = Array.from(children);
-    const getContentAndCloseShelf = shelf =>
-      setHeight(getContentElement(shelf));
-
-    return childrenArr.forEach(getContentAndCloseShelf);
-  }
-
-  function setHeight(element, value = 0) {
-    element.style.height = `${value}px`;
-  }
-
-  function getContentElement(element) {
-    return element.querySelector(`.${WORK_GROUP_CONTENT_CLASSNAME}`);
-  }
-
-  /**
-   * addIndexAttr
-   * Adds 'data-index={}` attribute to all accordion sections.
-   */
-  function addIndexAttr() {
-    const { children } = elements;
-
-    if (children.length === 0) {
-      return null;
-    }
-
-    const childrenArr = Array.from(children);
-
-    childrenArr.forEach((child, index) => {
-      child.setAttribute("data-index", index);
-    });
+    return expandSection(section);
   }
 
   function setBackgroundColor(color) {
     document.body.style.backgroundColor = color;
   }
 
-  function removeBackgroundColor() {
-    document.body.removeAttribute("style");
-  }
-
   function init() {
-    // Store the original heights of each section.
-    state.heights = getHeights();
-
-    // Add an index to the DOM so we know which one is which.
-    addIndexAttr();
-
-    // Set the height to 0 on all sections.
-    collapseHeightOfShelves();
-
-    // Make clicking do things.
-    setupEventListeners();
+    elements.accordion.addEventListener("click", handleClick, false);
   }
+
+  init();
 };
