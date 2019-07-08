daf6946e030954dc355b2d7accde27792c800daf
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-07-07 20:31:58 -0700

    hide scroll indicator if page is scrolled down on load already

--- scripts/toggleScrollIndicator.js
+++ scripts/toggleScrollIndicator.js
@@ -7,6 +7,14 @@ export const toggleScrollIndicator = () => {
   const element = document.querySelector('.intro-scroll-indicator');
   const TOGGLE_CLASSNAME = 'intro-scroll-indicator--hide';
 
+  /**
+   * If the page is loaded already scrolled down a bit
+   * then we hide this immediately.
+   */
+  if (window.scrollY > 10) {
+    element.classList.add(TOGGLE_CLASSNAME);
+  }
+
   window.addEventListener('scroll', () => {
     const { scrollY } = window;
 
